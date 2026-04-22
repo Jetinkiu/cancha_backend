420084578cb850a6fd70125dc599cd6a473f736b
Jetinkiu <lorenayumega@gmail.com>
2026-04-22 01:04:44 -0400

    Actualizar conexión a base de datos para producción

--- config/database.js
+++ config/database.js
@@ -1,29 +1,25 @@
-//cancha_backend/config/database.js
-const { Pool } = require('pg');
-
-const DB_NAME = process.env.DB_NAME || 'canchalic';
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  database: DB_NAME,
-  password: process.env.DB_PASSWORD || '123123',
-  port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-// ✅ Hacer una conexión inicial de prueba
+// conexión de prueba
 (async () => {
   try {
     const client = await pool.connect();
-    console.log(`Conectado a la base de datos ${DB_NAME}`);
+    console.log("✅ Conectado a la base de datos");
     client.release();
   } catch (err) {
-    console.error('❌ Error al conectar a la base de datos:', err.stack);
+    console.error("❌ Error al conectar a la base de datos:", err.message);
   }
 })();
 
-pool.on('error', (err) => {
-  console.error('Error en la conexión a la base de datos:', err.stack);
+pool.on("error", (err) => {
+  console.error("Error en la conexión a la base de datos:", err.message);
 });
 
 module.exports = pool;
